b6dacd3f6121f92461d35399041bb9e2aa3d033d
Jaum <j.calixtodegodoy@gmail.com>
2023-09-19 08:24:57 -0300

    add: pop up modal pokemon

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -13,23 +13,37 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.type = type
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
+    pokemon.abilities = pokeDetail.abilities.map((ability) => ability.ability.name);
     return pokemon
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+pokeApi.getPokemonDetail = async (pokemon) => {
+    try {
+        const response = await fetch(pokemon.url);
+        if (!response.ok) {
+            throw new Error('Unable to retrieve Pokémon details.');
+        }
+        const pokeDetail = await response.json();
+        return convertPokeApiDetailToPokemon(pokeDetail);
+    } catch (error) {
+        console.error('Unable to retrieve Pokémon details:', error);
+        throw error;
+    }
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+pokeApi.getPokemons = async (offset = 0, limit = 5) => {
+    try {
+        const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Unable to retrieve the list of Pokémon.');
+        }
+        const jsonBody = await response.json();
+        const pokemons = jsonBody.results;
+        const detailRequests = pokemons.map(pokeApi.getPokemonDetail);
+        return Promise.all(detailRequests);
+    } catch (error) {
+        console.error('Error while fetching the list of Pokémon:', error);
+        throw error;
+    }
 }
